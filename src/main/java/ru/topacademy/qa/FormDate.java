decf635c0d56b9a7987a566e9c805555a9ff1ea8
sovchikmi <marqsya@gmail.com>
2024-06-27 09:54:44 +0500

    first commit

---
+++ src/main/java/ru/topacademy/qa/FormDate.java
@@ -0,0 +1,7 @@
+package ru.topacademy.qa;
+
+public class FormDate {
+    public int day;
+    public int month;
+    public int year;
+}
---
+++ src/main/java/ru/topacademy/qa/Post.java
@@ -0,0 +1,11 @@
+package ru.topacademy.qa;
+
+public class Post {
+    public String name; // Имя
+    public String surname; // Фамилия
+    public String patronymic; // Отчество
+    public String phone; // Телефон
+    public String passport; // Паспорт
+    public boolean subscription; // Подписка
+    public FormDate birthday; // День рождения
+}
